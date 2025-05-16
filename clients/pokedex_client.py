016a69ae48ceea9af020028070b9080643deb6bf
michaelmaysonet74 <michaelmaysonet74@gmail.com>
2025-05-15 22:38:26 -0500

    wip

--- clients/pokedex_client.py
+++ clients/pokedex_client.py
@@ -1,6 +1,3 @@
-from models.pokemon import Pokemon
-from models.pokemon_type import PokemonType
-
 import requests
 
 
@@ -8,36 +5,16 @@ class PokedexClient:
     def __init__(self, base_url="http://localhost:8080"):
         self.base_url = base_url
 
-    def get_pokemon_by_id(self, id) -> Pokemon | None:
-        response = requests.get(f"{self.base_url}/api/v1/pokemon/{id}")
-        return (
-            self._parse_response(response.json())
-            if response.status_code == 200
-            else None
-        )
+    def get_pokemon_by_id(self, id) -> dict | None:
+        try:
+            response = requests.get(f"{self.base_url}/api/v1/pokemon/{id}")
+            return response.json() if response.status_code == 200 else None
+        except:
+            return None
 
-    def get_pokemon_by_name(self, name) -> Pokemon | None:
-        response = requests.get(f"{self.base_url}/api/v1/pokemon", {"name": name})
-        return (
-            self._parse_response(response.json())
-            if response.status_code == 200
-            else None
-        )
-
-    def _parse_response(self, json_data) -> Pokemon:
-        return Pokemon(
-            id=json_data["id"],
-            name=json_data["name"],
-            category=json_data["category"],
-            entry=json_data["entry"],
-            generation=json_data["generation"],
-            types=[
-                PokemonType(t)
-                for t in [
-                    json_data["types"]["primary"],
-                    json_data["types"]["secondary"],
-                ]
-                if t
-            ],
-            weaknesses=json_data["weaknesses"],
-        )
+    def get_pokemon_by_name(self, name) -> dict | None:
+        try:
+            response = requests.get(f"{self.base_url}/api/v1/pokemon", {"name": name})
+            return response.json() if response.status_code == 200 else None
+        except:
+            return None
--- services/pokemon_service.py
+++ services/pokemon_service.py
@@ -1,5 +1,6 @@
 from clients.pokedex_client import PokedexClient
 from models.pokemon import Pokemon
+from models.pokemon_type import PokemonType
 
 
 class PokemonService:
@@ -7,7 +8,27 @@ class PokemonService:
         self.pokedex_client = pokedex_client
 
     def get_pokemon_by_id(self, id: int) -> Pokemon | None:
-        return self.pokedex_client.get_pokemon_by_id(id)
+        maybe_response = self.pokedex_client.get_pokemon_by_id(id)
+        return self._parse_response(maybe_response) if maybe_response else None
 
     def get_pokemon_by_name(self, name: str) -> Pokemon | None:
-        return self.pokedex_client.get_pokemon_by_name(name)
+        maybe_response = self.pokedex_client.get_pokemon_by_name(name)
+        return self._parse_response(maybe_response) if maybe_response else None
+
+    def _parse_response(self, json_data: dict) -> Pokemon:
+        return Pokemon(
+            id=json_data["id"],
+            name=json_data["name"],
+            category=json_data["category"],
+            entry=json_data["entry"],
+            generation=json_data["generation"],
+            types=[
+                PokemonType(t)
+                for t in [
+                    json_data["types"]["primary"],
+                    json_data["types"]["secondary"],
+                ]
+                if t
+            ],
+            weaknesses=json_data["weaknesses"],
+        )
