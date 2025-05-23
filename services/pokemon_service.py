fb6b696e0c1fa0b7d01afe21d1d1d1a27e6b6e13
michaelmaysonet74 <michaelmaysonet74@gmail.com>
2025-05-23 13:47:48 -0500

    Handle error while parsing respone

--- services/pokemon_service.py
+++ services/pokemon_service.py
@@ -14,22 +14,25 @@ class PokemonService:
         maybe_response = self.pokedex_client.get_pokemon_by_name(name)
         return self._parse_response(maybe_response) if maybe_response else None
 
-    def _parse_response(self, json_data: dict) -> Pokemon:
-        return Pokemon(
-            id=json_data["id"],
-            name=json_data["name"],
-            category=json_data["category"],
-            entry=json_data["entry"],
-            generation=json_data["generation"],
-            types=[
-                t
-                for t in [
-                    json_data["types"]["primary"],
-                    # We can have a secondary type or not depending on the pokemon,
-                    # that's why we want to filter out the None values.
-                    json_data["types"]["secondary"],
-                ]
-                if t
-            ],
-            weaknesses=json_data["weaknesses"],
-        )
+    def _parse_response(self, json_data: dict) -> Pokemon | None:
+        try:
+            return Pokemon(
+                id=json_data["id"],
+                name=json_data["name"],
+                category=json_data["category"],
+                entry=json_data["entry"],
+                generation=json_data["generation"],
+                types=[
+                    t
+                    for t in [
+                        json_data["types"]["primary"],
+                        # We can have a secondary type or not depending on the pokemon,
+                        # that's why we want to filter out the None values.
+                        json_data["types"]["secondary"],
+                    ]
+                    if t
+                ],
+                weaknesses=json_data["weaknesses"],
+            )
+        except:
+            return None
