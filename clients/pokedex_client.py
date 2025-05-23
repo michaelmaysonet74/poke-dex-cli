be8e36d9e2694d366ee9028a03b461719aae9c67
michaelmaysonet74 <michaelmaysonet74@gmail.com>
2025-05-23 13:09:45 -0500

    wip

--- clients/pokedex_client.py
+++ clients/pokedex_client.py
@@ -9,7 +9,8 @@ class PokedexClient:
     def get_pokemon_by_id(self, id) -> dict | None:
         try:
             response = requests.get(
-                f"{self.base_url}/api/v1/pokemon/{id}", timeout=self.timeout
+                f"{self.base_url}/api/v1/pokemon/{id}",
+                timeout=self.timeout,
             )
             return response.json() if response.status_code == 200 else None
         except:
